1d304326a750ef9ea85d004a6ab8d6527170ee69
3R1CK24M <eradehielo120023@gmail.com>
2022-09-07 10:17:01 -0500

    Ulmimo avance dia uno

--- index.php
+++ index.php
@@ -4,31 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../img/SoftcompanyLogo.ico">  
-    <title>DMB</title>
+    <title>ensayo</title>
+    <link href="views/estilos.css" rel="stylesheet" >
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="views/estilos.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg" style="background-color: #FFAD56;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Dragon Match Battle</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                
+                <li class="nav-item">
+                <a class="nav-link" href="reglas.html">Reglas</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+                Ser el piloto de tu propio destino
+            </span>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        <div class="login"> 
-            <h2>Dragon Match Battle</h2>  
-            <form id="form2" method="POST">
-                <div class="mb-2">
-                    <input type="text" name="correo2" id="correo2" class="form-control" placeholder="Name">
+        <div class="form">
+            <h1 class="titulo">Ingreso</h1>
+            <form >
+                <div class="form-floating">
+                    <input type="text" class="form-control" placeholder="Ingrese su nombre aqui" >
+                    <label for="floatingTextarea">Ingresa tu nick</label>
                 </div>
-                <div class="mb-2">
-                    <button class="btn btn-info" type="submit" name="ingresar" id="ingresar" >Generar codigo</button>
+                <div class="elboton">
+                    <button type="submit" class="btn btn-info">Entrar</button>
                 </div>
-                <figure class="logo">
-                    <img src="obj/dbll.png" alt="logo">
-                </figure>
-            </form>  
+            </form>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 </body>
 </html>
